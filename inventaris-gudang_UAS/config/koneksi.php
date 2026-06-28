93cfd758f3a7f4a4834863df3011d82acdba916e
BramastraSatria-art <bramastrasatria666@gmail.com>
2026-06-28 14:00:59 +0800

    feat: tambah config database, session, dan validator

---
+++ inventaris-gudang_UAS/config/koneksi.php
@@ -0,0 +1,24 @@
+<?php
+class Database {
+    protected $conn;
+
+    public function __construct() {
+        $this->connect();
+    }
+
+    protected function connect() {
+        $host = "localhost";
+        $user = "root";
+        $pass = "";
+        $db   = "db_inventaris_gudang_uas";
+
+        $this->conn = new mysqli($host, $user, $pass, $db);
+
+        if ($this->conn->connect_error) {
+            die("Koneksi Error: " . $this->conn->connect_error);
+        }
+
+        $this->conn->set_charset("utf8mb4");
+    }
+}
+?>
